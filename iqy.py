d36e1c27af3f92bf82f9a8b4150289519fa50906
GoCigma <31538893+AKA-Cigma@users.noreply.github.com>
2024-10-08 00:30:19 +0800

    爱奇艺

--- iqy.py
+++ iqy.py
@@ -111,11 +111,11 @@ class IQiYi:
 
         try:
             if method == "GET":
-                response = self.session.get(url, headers=self.headers, params=body)
+                response = self.session.get(url, headers=self.headers, params=body, verify=False)
             elif method == "POST":
-                response = self.session.post(url, headers=self.headers, data=dumps(body))
+                response = self.session.post(url, headers=self.headers, data=dumps(body), verify=False)
             elif method == "OTHER":
-                response = self.session.get(url, headers=self.headers, params=dumps(body))
+                response = self.session.get(url, headers=self.headers, params=dumps(body), verify=False)
                 
             if method in ["GET", "POST"]:
                 data = response.json()
