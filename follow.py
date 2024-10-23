8e85f8fa731023fe182619c9874d97dc53e0bc92
GoCigma <31538893+AKA-Cigma@users.noreply.github.com>
2024-10-24 02:12:26 +0800

    Follow

---
+++ follow.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+# coding: utf-8
+"""
+项目名称: AKA-Cigma / QLscripts
+Author: AKA-Cigma
+功能：Follow自动签到
+抓Follow网页端的全部cookie填到环境变量'followck'里，多账号&连接
+Date: 2024/10/24
+cron: 8 0 * * *
+new Env('Follow');
+"""
+import requests
+import time
+import os
+try:
+    from notify import send
+except:
+    pass
+
+accounts = os.getenv('followck')
+
+if accounts is None:
+    print('未检测到followck')
+    exit(1)
+
+accounts_list = accounts.split('&')
+print(f"获取到 {len(accounts_list)} 个账号\n")
+
+urls = ["https://api.follow.is/auth/csrf",
+    "https://api.follow.is/wallets/transactions/claim-check",
+    "https://api.follow.is/wallets/transactions/claim_daily",
+    "https://api.follow.is/wallets",
+    "https://api.follow.is/wallets/transactions?fromOrToUserId=",
+]
+
+headers = {
+    'accept': '*/*',
+    'accept-language': 'zh-CN,zh;q=0.9,zh-TW;q=0.8,en-US;q=0.7,en;q=0.6',
+    'content-type': 'application/json',
+    'origin': 'https://app.follow.is',
+    'priority': 'u=1, i',
+    'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Windows"',
+    'sec-fetch-dest': 'empty',
+    'sec-fetch-mode': 'cors',
+    'sec-fetch-site': 'same-site',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36'
+}
+
+result = []
+
+for i, account in enumerate(accounts_list, start=1):
+    print(f"=======开始执行账号{i}=======\n")
+    cleaned_cookie = account
+    if 'authjs.callback-url=' in cleaned_cookie:
+        cleaned_cookie = cleaned_cookie.split('authjs.callback-url=')[0] + cleaned_cookie.split(';', 1)[1]
+    if 'authjs.csrf-token=' in cleaned_cookie:
+        cleaned_cookie = cleaned_cookie.split('authjs.csrf-token=')[0] + cleaned_cookie.split(';', 1)[1]
+    cleaned_cookie = '; '.join([c.strip() for c in cleaned_cookie.split(';') if c.strip()])
+
+    headers['cookie'] = cleaned_cookie
+    session = requests.Session()
+    session.headers.update(headers)
+    data = session.get(urls[0])
+    if data.text:
+        xCsrfToken = data.json()['csrfToken']
+        csrfToken = data.cookies.get_dict().get('authjs.csrf-token')
+        callback = data.cookies.get_dict().get('authjs.callback-url')
+    else:
+        result.append(f"账号{i}未获取到xCsrfToken：{data}\n")
+        continue
+
+    headers['cookie'] = cleaned_cookie + "; authjs.callback-url=" + callback + "; authjs.csrf-token=" + csrfToken
+    headers['x-csrf-token'] = xCsrfToken
+    session.headers.update(headers)
+    transactionHash = None
+
+    data = session.get(urls[1])
+    if data.text:
+        if not data.json()['data']:
+            result.append(f"账号{i}今日已签到！")
+        else:
+            data = session.post(urls[2])
+            if data.text and data.json()['code'] == 0:
+                result.append(f"账号{i}签到成功！")
+                transactionHash = data.json()['data']['transactionHash']
+                print("等待一分钟后刷新……")
+                time.sleep(60)
+            else:
+                result.append(f"账号{i}签到失败：{data}，")
+    else:
+        result.append(f"账号{i}查询签到状态失败：{data}")
+
+    data = session.get(urls[3])
+    if data.text:
+        userId = data.json()['data'][0]['userId']
+        powerToken = int(data.json()['data'][0]['powerToken']) / (10**18)
+        result.append(f"总power：{powerToken}，")
+    else:
+        result.append(f"查询power总数失败：{data}\n")
+        continue
+
+    data = session.get(urls[4] + userId)
+    if data.text:
+        if transactionHash and data.json()['data'][0]['hash'] == transactionHash:
+            result.append(f"签到校验成功！\n")
+        elif transactionHash is None:
+            result.append(f"仅在每天第一次签到校验结果。\n")
+        else:
+            result.append(f"签到校验失败：{data.json()['data'][0]['hash']}\n")
+    else:
+        result.append(f"签到校验失败：{data}\n")
+
+    session.close()
+
+try:
+    send("Follow签到",f"{''.join(result)}")
+except Exception as e:
+    print(f"消息推送失败：{e}！\n{result}\n")
